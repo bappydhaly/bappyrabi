699c2e8fa03373ec53167ca60672fcf9fabccab2
bappydhaly <133310969+bappydhaly@users.noreply.github.com>
2023-05-24 23:10:07 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://www.highrevenuegate.com/hj8uwz08a?key=5562a6d9a7f6f476dbb94100f559aac9://saveourstateok.org/`
 				}`,
 			},
 		};
